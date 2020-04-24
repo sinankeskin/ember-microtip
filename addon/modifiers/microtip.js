eb11a5ab8e757eb4ad45968edcf5e7ad65467dd1
Sinan Keskin <sinan@kesk.in>
2020-04-24 21:13:10 +0300

    First commit

---
+++ addon/modifiers/microtip.js
@@ -0,0 +1,22 @@
+import { isPresent } from '@ember/utils';
+import { modifier } from 'ember-modifier';
+
+export default modifier(function microtip(element, [label, position, size] /*, hash*/) {
+  if (isPresent(label)) {
+    element.setAttribute('aria-label', label);
+
+    if (isPresent(position)) {
+      element.setAttribute('data-microtip-position', position);
+    } else {
+      element.setAttribute('data-microtip-position', 'top');
+    }
+
+    if (isPresent(size)) {
+      element.setAttribute('data-microtip-size', size);
+    } else {
+      element.setAttribute('data-microtip-size', 'fit');
+    }
+
+    element.setAttribute('role', 'tooltip');
+  }
+});
---
+++ tests/integration/modifiers/microtip-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Modifier | microtip', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    await render(hbs`<div {{microtip}}></div>`);
+
+    assert.ok(true);
+  });
+});
