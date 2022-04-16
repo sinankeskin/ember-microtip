773b9c52b818202a87bf1fa743c7858a4613892b
Sinan Keskin <sinan@kesk.in>
2022-04-16 18:49:21 +0300

    Fix deprecation messages

--- addon/modifiers/microtip.js
+++ addon/modifiers/microtip.js
@@ -1,25 +1,25 @@
 import { isPresent } from '@ember/utils';
 import { modifier } from 'ember-modifier';
 
-export default modifier(function microtip(
-  element,
-  [label, position, size] /*, hash*/
-) {
-  if (isPresent(label)) {
-    element.setAttribute('aria-label', label);
+export default modifier(
+  (element, [label, position, size]) => {
+    if (isPresent(label)) {
+      element.setAttribute('aria-label', label);
 
-    if (isPresent(position)) {
-      element.setAttribute('data-microtip-position', position);
-    } else {
-      element.setAttribute('data-microtip-position', 'top');
+      if (isPresent(position)) {
+        element.setAttribute('data-microtip-position', position);
+      } else {
+        element.setAttribute('data-microtip-position', 'top');
+      }
+
+      if (isPresent(size)) {
+        element.setAttribute('data-microtip-size', size);
+      } else {
+        element.setAttribute('data-microtip-size', 'fit');
+      }
+
+      element.setAttribute('role', 'tooltip');
     }
-
-    if (isPresent(size)) {
-      element.setAttribute('data-microtip-size', size);
-    } else {
-      element.setAttribute('data-microtip-size', 'fit');
-    }
-
-    element.setAttribute('role', 'tooltip');
-  }
-});
+  },
+  { eager: false }
+);
